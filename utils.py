9f7a74dcb68c7a277d68be4de70f7f4524700d79
WuJie <33804309+WuJie1010@users.noreply.github.com>
2018-07-15 14:44:29 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,72 @@
+'''Some helper functions for PyTorch, including:
+    - progress_bar: progress bar mimic xlua.progress.
+    - set_lr : set the learning rate
+    - clip_gradient : clip gradient
+'''
+
+import os
+import sys
+import time
+import math
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from torch.autograd import Function
+
+_, term_width = os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+
+TOTAL_BAR_LENGTH = 30.
+last_time = time.time()
+begin_time = last_time
+
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+
+    L = []
+    if msg:
+        L.append(' | ' + msg)
+
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+def set_lr(optimizer, lr):
+    for group in optimizer.param_groups:
+        group['lr'] = lr
+
+def clip_gradient(optimizer, grad_clip):
+    for group in optimizer.param_groups:
+        #print(group['params'])
+        for param in group['params']:
+            param.grad.data.clamp_(-grad_clip, grad_clip)
